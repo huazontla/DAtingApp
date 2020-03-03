a6b8f0753b366084ebbe5321a226e8fc1b1bf478
huazontla <oswaldoag2614@gmail.com>
2020-03-03 10:05:08 -0600

    Nuevos cambios, agregando la rutas

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit {
   // this will store our username and password
   model: any  = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -23,10 +24,12 @@ export class NavComponent implements OnInit {
     }, error => {
       console.log(error);
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/messages']);
     });
   }
 
-  loggedIn(){
+  loggedIn() {
     // const token = localStorage.getItem('token');
 
     // return !!token; // if this token variable is empty then it's going to return false
@@ -34,9 +37,10 @@ export class NavComponent implements OnInit {
     return this.authService.loggedIn();
   }
 
-  loggedOut(){
+  loggedOut() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home'])
   }
 
 }
