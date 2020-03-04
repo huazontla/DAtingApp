1567d3cdb5fe25b0f1cd7dd5a314f83feda1d7ab
huazontla <oswaldoag2614@gmail.com>
2020-03-03 21:48:17 -0600

    Final de la seccion 7 (Agregando rutas y AuthGuard)

--- DatingApp-SPA/src/app/_guards/auth.guard.ts
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate} from '@angular/router';
+import { CanActivate, Router} from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router, private alertify: AlertifyService) {}
+
   canActivate(): boolean  {
-    return true;
+    if (this.authService.loggedIn()) {
+      return true;
+    }
+    this.alertify.error('Largate de aqui');
+    this.router.navigate(['/home']);
+    return false;
   }
 }
